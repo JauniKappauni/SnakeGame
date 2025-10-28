98acf4f6548ee07cc3f9b87af7f96f9b8dd13168
JauniKappauni <social.jauni@gmail.com>
2025-10-28 16:24:08 +0100

    perf: enable double buffering to reduce flickering

--- Form1.cs
+++ Form1.cs
@@ -20,6 +20,7 @@ namespace SnakeGame
         public Form1()
         {
             InitializeComponent();
+            this.DoubleBuffered = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
