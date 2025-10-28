3a7dad0255284f06a0711b8941ded9556b142e12
JauniKappauni <social.jauni@gmail.com>
2025-10-28 14:45:01 +0100

    feat: create Snake class with body list and direction fields

---
+++ Snake.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    public class Snake
+    {
+        public List<Point> Body;
+        public int DirX;
+        public int DirY;
+
+        public Snake(int startX, int startY)
+        {
+            Body = new List<Point>();
+            Body.Add(new Point(startX, startY));
+            DirX = 1;
+            DirY = 0;
+        }
+        void Move()
+        {
+
+        }
+        void Grow()
+        {
+
+        }
+        void ChangeDirection()
+        {
+        }
+        void IsCollision()
+        {
+        }
+    }
+}
