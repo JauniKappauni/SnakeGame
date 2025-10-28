819691b69303efb2c58b4853c5316ac16bca72e3
JauniKappauni <social.jauni@gmail.com>
2025-10-28 14:58:28 +0100

    feat: add Food class

---
+++ Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    internal class Food
+    {
+        public Point Pos;
+        public int Value;
+        public Brush Color;
+        public Food(int x, int y, int value, Brush color)
+        {
+            Pos = new Point(x, y);
+            Value = value;
+            Color = color;
+        }
+    }
+}
